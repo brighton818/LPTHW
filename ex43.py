6438a50b327e5eac381ba9f54a02a4fa429313a1
BrightonKim <brightonkim@seoulchristian.org>
2016-02-15 04:49:05 +0900

    Finish checkpoint three

--- ex43.py
+++ ex43.py
@@ -1,21 +1,36 @@
+from sys import exit
+
 class Scene(object):
 
     def enter(self):
+        print "Not yet classified."
+        exit(0)
         pass
 
 
 class Engine(object):
 
     def __init__(self, scene_map):
+        self.scene_map = scene_map
         pass
 
     def play(self):
+        present_scene = self.scene_map.opening_scene()
+        final_scene = self.scene_map.next_scene('finish')
+
+        while present_scene != final_scene:
+            next_scene_name = present_scene.enter()
+            present_scene = self.scene_map.next_scene(next_scene_name)
+
+        present_scene.enter()
         pass
 
 
 class Death(Scene):
 
     def enter(self):
+        print "You have died because of your imperfect holiness and the Christian Armor."
+        exit(0)
         pass
 
 
@@ -58,4 +73,3 @@ class Map(object):
 a_map = Map('central_corridor')
 a_game = Engine(a_map)
 a_game.play()
-
